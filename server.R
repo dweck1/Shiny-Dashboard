35c8021b176232e09993397819e2c711c2bc6cb7
David Weck <daweck@ncsu.edu>
2020-07-25 17:03:26 -0500

    Created EDA and Clustering tabls

--- server.R
+++ server.R
@@ -1,10 +1,139 @@
 library(shiny)
 library(shinydashboard)
-library(tidyverse)
-library(plotly)
-library(png)
+library(DT)
 
-# Define server logic required to draw a histogram
+source('analysis.R')
+
 shinyServer(function(input, output, session) {
 
+#EDA TAB ----------------------------------------------
+  eda_team_data <- reactive({
+    eda_team_data <- NFL %>% filter(team == input$eda_team)
+  })
+    
+  #Boxplot on EDA tab
+  output$boxplot <- renderPlotly({
+    
+    if(!input$team_bool){
+    
+      boxplot <- plot_ly(NFL, y = ~wins_next_year, type = 'box', color = ~team, colors= ~primary)
+      boxplot <- boxplot %>% layout(xaxis = list(title = 'Team'), yaxis = list(title = 'Wins'))
+      boxplot
+    }
+    else{
+      boxplot <- plot_ly(eda_team_data(), y = ~wins_next_year, type = 'box', color = ~team, colors= ~primary)
+      boxplot <- boxplot %>% layout(yaxis = list(title = 'Wins'))
+      boxplot
+    }
+  })
+  
+  
+  #Scatterplot on EDA tab
+  output$scatter <- renderPlotly({
+    
+    if(!input$team_bool){
+      scatter <- plot_ly(NFL, x = ~get(input$eda_var1), y = ~wins_next_year + rnorm(nrow(NFL), 0, .25),
+                         type = 'scatter', mode = 'markers', color = ~team, colors = ~primary)
+      scatter <- scatter %>% layout(xaxis = list(title = str_to_title(str_replace_all(input$eda_var1, '_', ' '))), 
+                                    yaxis = list(title = 'Wins Next Year'))
+      scatter
+    }
+    else{
+      scatter <- plot_ly(eda_team_data(), x = ~get(input$eda_var1), y = ~wins_next_year + rnorm(nrow(eda_team_data()), 0, .25),
+                         type = 'scatter', mode = 'markers', color = ~team, colors = ~secondary, size = I(100))
+      scatter <- scatter %>% layout(xaxis = list(title = str_to_title(str_replace_all(input$eda_var1, '_', ' '))),
+                                    yaxis = list(title = 'Wins Next Year'))
+      scatter
+    }
+
+  })
+  
+  #Title for scatterplot
+  output$scatter_title <- renderUI({
+    paste('Scatterplot of Wins Next Year vs', str_to_title(str_replace_all(input$eda_var1,'_', ' ')), '(Jitter Added)')
+    })
+  
+  #Summary stats for wins
+  output$wins_table <- renderTable({
+    
+    if(!input$team_bool){
+      
+      t(as.matrix(summary(NFL$wins_next_year)))
+      
+    }
+    else{
+      t(as.matrix(summary(eda_team_data()$wins_next_year)))
+    }
+  })
+  
+  #Histogram for predictor variables
+  output$hist <- renderPlotly({
+    
+    if(!input$team_bool){
+      
+      hist <- plot_ly(NFL, x = ~get(input$eda_var1), type = 'histogram')
+      hist <- hist %>% layout(xaxis = list(title = str_to_title(str_replace_all(input$eda_var1, '_', ' '))),
+                              yaxis = list(title = ''))
+      hist
+      
+    }
+    else{
+      hist <- plot_ly(eda_team_data(), x = ~get(input$eda_var1), 
+                      type = 'histogram', color = ~team, colors = ~secondary, nbinsx = 20)
+      hist <- hist %>% layout(xaxis = list(title = str_to_title(str_replace_all(input$eda_var1, '_', ' '))),
+                              yaxis = list(title = ''))
+      hist
+    }
+  })
+  
+  #Title for histogram
+  output$hist_title <- renderUI({
+    paste('Histogram of', str_to_title(str_replace_all(input$eda_var1, '_', ' ')))
+  })
+  
+  
+#Clustering Tab ---------------------------------------
+  
+  cluster_data <- reactive({
+    NFL[, c(input$clustering_var1, input$clustering_var2)]
+  })
+  
+  kmeans_clust <- reactive({
+    kmeans(cluster_data(), centers = input$n_clusters)
+  })
+  
+  output$clust_plot <- renderPlotly({
+    
+    clust_plot <- plot_ly(cluster_data(), x = ~get(input$clustering_var1), y = ~get(input$clustering_var2),
+                          type = 'scatter', mode = 'markers',
+                          color = kmeans_clust()$cluster, 
+                          colors = c('#F5F91F', '#1F7EF9', '#F9261F', '#1FF92A', '#F91FE1',
+                                     '#F99211', '#F9117D', '#11F9EB', '#192573', '#D81D15'),
+                          size = I(50)) %>%
+                  add_markers(kmeans_clust()$centers[,1], kmeans_clust()$centers[,2],
+                              symbol = I(8), 
+                              color = I('Black'),
+                              alpha = I(1),
+                              size = I(2000)) %>%
+                  layout(xaxis = list(title = str_to_title(str_replace_all(input$clustering_var1, '_', ' '))),
+                         yaxis = list(title = str_to_title(str_replace_all(input$clustering_var2, '_', ' '))),
+                         showlegend = FALSE) %>%
+                  hide_colorbar()
+  })
+  
+  hclustering <- reactive({
+    hclust(dist(cluster_data()), method = input$linkage)
+  })
+  
+  output$dendrogram <- renderPlot({
+    plot(as.dendrogram(hclustering()), xlab = 'Height', ylab = '', main = 'Dendrogram',
+         nodePar = list(pch = 19, col = 2, cex = .2),
+         edgePar = list(col = 4),
+         leaflab = 'none',
+         horiz = TRUE)
+  })
+  
+#Modelling Tab ----------------------------------------
+  
 })
+
--- ui.R
+++ ui.R
@@ -1,55 +1,100 @@
 library(shiny)
 library(shinydashboard)
 
+source('analysis.R')
+
 dashboardPage(
     
+# Header ----------------------------------------------
     dashboardHeader(title = 'NFL Win Predictions'),
-    
+
+#Sidebar ----------------------------------------------
     dashboardSidebar(
         sidebarMenu(id = 'sidebar',
             
             menuItem('Info',  
                      tabName = 'info', 
                      icon = icon('info-circle')
-            ),
+                    ),
             
             menuItem('EDA', 
                      tabName = 'eda', 
                      icon = icon('chart-bar')
-                     
-            ),
+                    ),
+                conditionalPanel('input.sidebar == "eda"',
+                    selectizeInput('eda_var1',
+                                    'Select a predictor to explore',
+                                    choices = colnames(x_vars),
+                                    selected = 'points_scored'
+                                    ),
+                    checkboxInput('team_bool',
+                                   'Explore a specific team'
+                                  ),
+                    conditionalPanel('input.team_bool',
+                                     selectizeInput('eda_team',
+                                                    'Select a team to explore',
+                                                    choices = as.factor(NFL$team),
+                                                    selected = 'GB')
+                                     )
+                                ),
+            
             
             menuItem('Clustering', 
                      tabName = 'clustering', 
                      icon = icon('object-group')
-            ),
+                    ),
+                conditionalPanel('input.sidebar == "clustering"',
+                     selectizeInput('clustering_var1',
+                                    'Select the X variable',
+                                    choices = colnames(x_vars),
+                                    selected = 'points_scored'
+                                    ),
+                     selectizeInput('clustering_var2',
+                                    'Select the Y variable',
+                                    choices = colnames(x_vars),
+                                    selected = 'yards_per_play'
+                                    ),
+                     sliderInput('n_clusters',
+                                 'Select the number of clusters',
+                                 min = 1,
+                                 max = 10,
+                                 value = 4
+                                 ),
+                     selectizeInput('linkage',
+                                    'Select linkage for hierarchical clustering',
+                                    choices = c('complete', 'single', 'average'),
+                                    selected = 'complete'),
+                     ),
             
             menuItem('Modelling', 
                      tabName = 'modelling', 
                      icon = icon('project-diagram')
-            ),
+                    ),
             
             menuItem('Data', 
                      tabName = 'data', 
                      icon = icon('table')
-            )
+                    )
             
-        )
+                )
     ),
-    
+
+# Body ------------------------------------------------
     dashboardBody(
         tabItems(
             
+            #- Info Tab--------------------------------
             tabItem('info', 
                 fluidRow(
                     box(
                         title = 'About the Data',
                         width = "6 col-lg-4",
+                        height = 570,
                         solidHeader = TRUE,
                         status = 'info',
                         p(class = 'text-center', img(src = 'nfl_teams.png', height = 200, width = 200)
                           ),
-                        p('The data set used in this dashboard consists of season wins, losses, and various offensive',
+                        p('The data set used in the dashboard consists of season wins, losses, and various offensive',
                           'and defensive statistics for each NFL team dating back to the 1999 season. The full,',
                           'uncleaned dataset can be found', 
                           a(href = 'https://www.kaggle.com/ttalbitt/american-football-team-stats-1998-2019', 'here.'),
@@ -66,24 +111,28 @@ dashboardPage(
                     box(
                         title = 'About the Dashboard',
                         width = "6 col-lg-4",
+                        height = 570,
                         solidHeader = TRUE,
                         status = 'info',
                         p(class = 'text-center', img(src = 'shiny2.webp', height = 200, width = 350)
                         ),
-                        p('Beyond this home page, the dashboard consists of 4 interative pages. On the',strong("EDA"),
+                        p('Beyond the home page, the dashboard consists of 4 interactive pages. On the',strong("EDA"),
                           'tab the user can generate various graphical and numerical summaries for the entire',
-                          'league, a single division, or a specific team. On the', strong("Clustering"), 'tab, the',
-                          'user can perform K-Means clustering using selected variables and a selected number of',
-                          'clusters. A plot displays the clusters and centroids created from the selected options.',
-                          'On the', strong("Modelling"), 'tab, there are options to create LASSO and random forest',
-                          'models to predict the total number of wins a team will win next year. The user has the',
-                          'option to adjust certain tuning parameters for each model. Finally, on the', 
-                          strong("Data"), 'tab, the user can view and download the entire dataset or a dataset',
-                          'filtered by team.')
+                          'league or for a selected team. On the', strong("Clustering"), 'tab, the user',
+                          'can perform K-Means using selected variables and a selected number of clusters. A plot',
+                          'displays the clusters and centroids created from the selected K-Means options. The user',
+                          'can also perform hierarchical clustering and select which linkage method they would like.',
+                          'A dendrogram is plotted with the result. On the', strong("Modelling"), 'tab, there',
+                          'are options to create LASSO and Random Forest models to predict the total number of games',
+                          'a team will win next year. The user has the option to adjust certain tuning parameters',
+                          'for each model. Finally, on the', strong("Data"), 'tab, the user can view and download',
+                          'the entire dataset or a dataset filtered by team.')
                     ),
+                    
                     box(
                         title = 'About the Creator',
                         width = "6 col-lg-4",
+                        height = 570,
                         solidHeader = TRUE,
                         status = 'info',
                         p(class = 'text-center', img(src = 'Capture.png', height = 200, width = 225)
@@ -98,7 +147,7 @@ dashboardPage(
                         'and the NFL. I stick to my undergraduate roots when it comes to my college football team.',
                         'I was born and raised a Florida Gator fan and bleed', span("orange", style = "color:orange"),
                         'and', span("blue.", style = "color:blue"), 'As for the NFL, my team is the Green Bay',
-                        'Packers. '
+                        'Packers.'
                         ),
                         p('I had a lot of fun creating this application and hope you find it interesting to play',
                         'around with.', strong("Thank you!")
@@ -107,8 +156,38 @@ dashboardPage(
                 )
             ),
             
+            # EDA Tab ---------------------------------
             tabItem('eda',
-                    p('EDA'))
+                    h2('Graphical and Numerical Summaries of NFL Data'),
+                    fluidRow(
+                        column(width = 6, 
+                               h4("Boxplot of Wins each Year", align = 'center'), 
+                               plotlyOutput('boxplot')),
+                        column(width = 6, 
+                               h4(uiOutput('scatter_title'), align = 'center'), 
+                               plotlyOutput('scatter'))
+                    ),
+                    fluidRow(
+                        column(width = 4,
+                               h4('Summary Statistics for Wins', align = 'center'),
+                               tableOutput('wins_table')),
+                        column(width = 2, 
+                               h4('How to download a plot', align = 'center'),
+                               p('1. Hover over plot you want to download'),
+                               p('2. Locate menu icons in top right'),
+                               p('3. Click on the camera icon (far left icon)')),
+                        column(width = 6,
+                               h4(uiOutput('hist_title'), align = 'center'),
+                               plotlyOutput('hist'))
+                    )
+            ),
+            
+            # Clustering tab --------------------------
+            tabItem('clustering',
+                    h2('K-Means Clustering of 2 Predictors'),
+                    plotlyOutput('clust_plot'),
+                    h2('Dendrogram from Hierarchical Clustering of the 2 Predictors'),
+                    plotOutput('dendrogram'))
         )
     )
 )
